8877c3e01106ce775adb9e3a5fdb7118a55464ce
samboy29@gmail.com <samboy29@gmail.com>
2022-02-07 21:27:03 +1000

    Added the ability to create new users via api

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+
+  def create
+    user = User.create!(
+      validate_params
+    )
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        user: user
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  private
+
+  def validate_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
